113a97da1ccd0c5adf8685c07f5968aa70e4800b
yeray <yeray.cabello@codurance.com>
2022-12-16 14:52:12 +0100

    comments

--- backend/src/FunctionalTodo/FunctionalTodo/DomainModel/FunctionTypes.cs
+++ backend/src/FunctionalTodo/FunctionalTodo/DomainModel/FunctionTypes.cs
@@ -1,7 +1,15 @@
-﻿using FunctionalTodo.Models;
+﻿using DeFuncto;
+using FunctionalTodo.Models;
 
 namespace FunctionalTodo.DomainModel;
 
 public delegate Task<IEnumerable<TodoListItem>> GetAllFromDb();
 public delegate Task<int> CreateTodo(TodoCreation parameters);
+//Para Irene
+// docker compose up EN LA CARPETA DONDE ESTá EL YAML
+// crea la base de datos "todo" a mano.
+public delegate Task<TodoListItem> GetByTitle(string title);
+public delegate AsyncOption<TodoListItem> FindByTitle(string title);
+
+
 public delegate string GetConnectionString();
