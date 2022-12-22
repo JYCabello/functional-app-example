62ac9258b935ae82baf334e803a4994b0507479a
yeray <yeray.cabello@codurance.com>
2022-12-22 19:10:06 +0100

    using a record

--- backend/src/FunctionalTodo/FunctionalTodo/Models/TodoListItem.cs
+++ backend/src/FunctionalTodo/FunctionalTodo/Models/TodoListItem.cs
@@ -1,8 +1,3 @@
 ﻿namespace FunctionalTodo.Models;
 
-public class TodoListItem
-{
-    public int ID { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public bool IsCompleted { get; set; }
-}
+public record TodoListItem(int ID, string Title, bool IsCompleted);
